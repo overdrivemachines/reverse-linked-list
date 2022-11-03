b27f561567102d932ceda73edcde96b78e852f04
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-02 22:14:45 -0700

    added create and print

--- reverse-linked-list.cpp
+++ reverse-linked-list.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -11,11 +12,49 @@ struct ListNode {
   ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
+ListNode* createLinkedList(vector<int> values);
+void printLinkedList(ListNode* head);
 ListNode* reverseList(ListNode* head);
 
 int main(int argc, char const* argv[]) {
+  vector<int> v1 = {5, 32, 1, 2, 93, 75, 89};
+  vector<int> v2 = {6, 2, 30, 34};
+
+  ListNode* list1 = createLinkedList(v1);
+  ListNode* list2 = createLinkedList(v2);
+
+  printLinkedList(list1);
+  printLinkedList(list2);
   return 0;
 }
 
 ListNode* reverseList(ListNode* head) {
+  vector<ListNode*> addresses;
+  return head;
+}
+
+// Create Linked list given a vector of int values
+// returns pointer to head of lined list
+ListNode* createLinkedList(vector<int> values) {
+  ListNode* head;
+  ListNode* node = new ListNode();
+  head = node;
+  for (int value : values) {
+    node->next = new ListNode(value);
+    node = node->next;
+  }
+  head = head->next;
+  return head;
+}
+
+// Prints Linked List
+// e.g. 5->8->3->NULL
+void printLinkedList(ListNode* head) {
+  ListNode* node = head;
+
+  while (node != nullptr) {
+    cout << node->val << "->";
+    node = node->next;
+  }
+  cout << "NULL\n";
 }
