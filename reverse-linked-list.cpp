7f0bac9f7f75478da493ebd555a742dc838ec431
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-02 21:55:36 -0700

    added listnode struct

--- reverse-linked-list.cpp
+++ reverse-linked-list.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+// Definition for singly-linked list.
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+ListNode* reverseList(ListNode* head);
+
+int main(int argc, char const* argv[]) {
   return 0;
+}
+
+ListNode* reverseList(ListNode* head) {
 }
